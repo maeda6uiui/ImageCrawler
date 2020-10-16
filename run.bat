a6c28ebf7618a29ff9d5d8ea9c3fb38397ba8483
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-10-16 21:15:13 +0900

    一部のファイルをGitの管理から除外

--- run.bat
+++
@@ -1,5 +0,0 @@
-python .\crawl_images.py ^
-    --resume_index 0 ^
-    --feeder_threads 2 ^
-    --parser_threads 4 ^
-    --downloader_threads 8
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 ./crawl_images.py \
-    --resume_index 0 \
-    --feeder_threads 4 \
-    --parser_threads 8 \
-    --downloader_threads 16
