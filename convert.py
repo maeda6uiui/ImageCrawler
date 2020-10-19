93f255f826f5eada41fcf090406d07b0bb1d8c0e
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2020-10-19 22:54:50 +0900

    画像フォーマットの変換を行うコードを追加

---
+++ convert.py
@@ -0,0 +1,59 @@
+"""
+画像フォーマットの変換およびリサイズを行う。
+"""
+import argparse
+import glob
+import logging
+import os
+from PIL import Image
+from tqdm import tqdm
+
+logging_fmt = "%(asctime)s %(levelname)s: %(message)s"
+logging.basicConfig(format=logging_fmt)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+
+def main(save_root_dir,image_width,image_height,resume_index):
+    pathname=os.path.join(save_root_dir,"*")
+    directories=glob.glob(pathname)
+    for idx,directory in tqdm(enumerate(directories),total=len(directories)):
+        if idx<resume_index:
+            continue
+
+        pathname=os.path.join(directory,"*[!txt]")
+        files=glob.glob(pathname)
+
+        for file in files:
+            splits=os.path.splitext(file)
+            if splits[1]==".jpg":
+                continue
+
+            dst_file=splits[0]+".jpg"
+            try:
+                image=Image.open(file)
+
+                #Convert the image to RGB.
+                if image.mode in ("RGBA","P"):
+                    image=image.convert("RGB")
+
+                #Resize
+                if image.width!=image_width or image.height!=image_height:
+                    image=image.resize((image_width,image_height))
+                    
+                image.save(dst_file)
+
+            except Exception as e:
+                os.remove(file)
+                logger.error(e)
+
+                continue
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--save_root_dir",type=str,default="./Images")
+    parser.add_argument("--image_width",type=int,default=256)
+    parser.add_argument("--image_height",type=int,default=256)
+    parser.add_argument("--resume_index",type=int,default=0)
+    args=parser.parse_args()
+
+    main(args.save_root_dir,args.image_width,args.image_height,args.resume_index)
